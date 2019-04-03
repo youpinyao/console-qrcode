5f5cc3dc5d554e7b08c16b75dbde9f077891ae46
游品尧 <497400448@qq.com>
2019-04-03 17:06:53 +0800

    fix: const 变量重复声明

--- extension/browser.js
+++ extension/browser.js
@@ -1,21 +1,27 @@
-const qrcodeElement = document.createElement("div");
+(function() {
+  var qrcodeElement = document.createElement("div");
 
-var qrcode = new QRCode(qrcodeElement, {
-  text: window.location.href,
-  width: 256,
-  height: 256,
-  colorDark: "#000000",
-  colorLight: "#ffffff",
-  correctLevel: QRCode.CorrectLevel.H
-});
+  var qrcode = new QRCode(qrcodeElement, {
+    text: window.location.href,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+  });
 
-const imgElement = qrcodeElement.querySelector('img');
+  const imgElement = qrcodeElement.querySelector("img");
 
-function consoleQrcode() {
-  const styleText = `background:url('${qrcodeElement.querySelector('img').getAttribute('src')}') no-repeat; padding-bottom: ${imgElement.height}px; padding-left: ${imgElement.width}px`;
-  console.log('%c ', styleText);
-}
+  function consoleQrcode() {
+    const styleText = `background:url('${qrcodeElement
+      .querySelector("img")
+      .getAttribute("src")}') no-repeat; padding-bottom: ${
+      imgElement.height
+    }px; padding-left: ${imgElement.width}px`;
+    console.log("%c ", styleText);
+  }
 
-qrcodeElement.querySelector('img').addEventListener('load', consoleQrcode)
+  qrcodeElement.querySelector("img").addEventListener("load", consoleQrcode);
 
-window.consoleQrcode = consoleQrcode;
+  window.consoleQrcode = consoleQrcode;
+})();
